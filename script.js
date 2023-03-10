a3b23cfae36d544077835868c57dad61c480b3aa
Shizic <kyleshizic@gmail.com>
2023-03-10 18:41:43 +0100

    commit01

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 25,
+    loop: true
+})
